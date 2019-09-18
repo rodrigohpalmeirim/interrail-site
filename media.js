b7a8852e35d89a8b3f92e9bdc4f41abe07271d92
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2019-09-18 13:05:00 +0100

    add transitions

--- media.js
+++ media.js
@@ -1,13 +1,17 @@
 function big(src) {
-    document.getElementById("media-viewer").style.display = "block";
-    document.getElementById("big-photo").style.display = "block";
     document.getElementById("big-photo").src = src;
+    document.getElementById("media-viewer").style.display = "block";
+    setTimeout(() => {document.getElementById("media-viewer").style.opacity = 1;}, 1);
+    document.getElementById("big-photo").style.display = "block";
+    setTimeout(() => {document.getElementById("big-photo").style.opacity = 1;}, 1);
 }
 
 document.onkeydown = function (e) {
     e = e || window.event;
     if (e.key == "Escape") {
-        document.getElementById("media-viewer").style.display = "none";
-        document.getElementById("big-photo").style.display = "none";
+        document.getElementById("media-viewer").style.opacity = 0;
+        setTimeout(() => {document.getElementById("media-viewer").style.display = "none";}, 200);
+        document.getElementById("big-photo").style.opacity = 0;
+        setTimeout(() => {document.getElementById("big-photo").style.display = "none";}, 200);
     }
 };
