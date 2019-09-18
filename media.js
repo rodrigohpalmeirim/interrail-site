71d4fb37b7e13d33037af8db314820a922d33cee
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2019-09-18 01:35:14 +0100

    remane media-viewer

--- media.js
+++ media.js
@@ -1,5 +1,5 @@
 function big(src) {
-    document.getElementById("big-media").style.display = "block";
+    document.getElementById("media-viewer").style.display = "block";
     document.getElementById("big-photo").style.display = "block";
     document.getElementById("big-photo").src = src;
 }
@@ -7,7 +7,7 @@ function big(src) {
 document.onkeydown = function (e) {
     e = e || window.event;
     if (e.key == "Escape") {
-        document.getElementById("big-media").style.display = "none";
+        document.getElementById("media-viewer").style.display = "none";
         document.getElementById("big-photo").style.display = "none";
     }
 };
