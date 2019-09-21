0313b444b23afdf2a0666071068cb70a7cbd5ce9
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2019-09-21 01:56:32 +0100

    add video transitions and clock

--- earth.js
+++ earth.js
@@ -1,45 +1,78 @@
 var place = 0;
 var placeVideo = document.getElementById("place-video");
 var transitionVideo = document.getElementById("transition-video");
+var reversedTransitionVideo = document.getElementById("reversed-transition-video");
 var transitioning = false;
+var timeouts = [];
+
+setClock(Date.parse(places[place].arrival.date+" "+places[place].arrival.time))
+
+placeVideo.addEventListener("loadeddata", function() {placeVideo.play()});
 
 function arrowKeyControl() {
     document.onkeydown = function (e) {
         e = e || window.event;
         if (e.key == "ArrowRight") {
             if (transitioning) {
+                setClock(Date.parse(places[place+1].arrival.date+" "+places[place+1].arrival.time.split("+")[0]))
+                stopClock();
                 placeVideo.src = places[++place].video;
-                transitionVideo.style.display = "none";
+                placeVideo.play();
+                transitionVideo.style.opacity = 0;
+                reversedTransitionVideo.style.opacity = 0;
+                if (place < places.length-1) transitionVideo.src = places[place+1].transitionVideo;
+                reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";
                 transitioning = false;
-                clearTimeout(timeout);
-            } else {
+                clearTimeouts();
+            } else if (place < places.length-1) {
+                fastForward(Date.parse(places[place].departure.date+" "+places[place].departure.time.split("+")[0]), Date.parse(places[place+1].arrival.date+" "+places[place+1].arrival.time.split("+")[0]))
                 transitioning = true;
-                transitionVideo.src = places[place+1].transitionVideo;
-                placeVideo.src = places[place+1].video;
-                transitionVideo.style.display = "block";
-                timeout = setTimeout(() => {
+                transitionVideo.play();
+                transitionVideo.style.opacity = 1;
+                timeouts.push(setTimeout(() => {placeVideo.src = places[place+1].video;}, 500));
+                timeouts.push(setTimeout(() => {
                     place++;
-                    transitionVideo.style.display = "none";
+                    placeVideo.play();
+                    transitionVideo.style.opacity = 0;
+                    if (place < places.length-1) setTimeout(() => {transitionVideo.src = places[place+1].transitionVideo;}, 500);
+                    reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";
                     transitioning = false;
-                }, 5000);
+                }, 5000));
             }
         } else if (e.key == "ArrowLeft") {
             if (transitioning) {
+                setClock(Date.parse(places[place].arrival.date+" "+places[place].arrival.time.split("+")[0]))
+                stopClock();
                 placeVideo.src = places[place].video;
-                transitionVideo.style.display = "none";
+                placeVideo.play();
+                transitionVideo.style.opacity = 0;
+                reversedTransitionVideo.style.opacity = 0;
+                transitionVideo.src = places[place+1].transitionVideo;
+                if (place > 0) reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";
                 transitioning = false;
-                clearTimeout(timeout);
-            } else {
+                clearTimeouts();
+            } else if (place > 0) {
+                fastForward(Date.parse(places[place].arrival.date+" "+places[place].arrival.time.split("+")[0]), Date.parse(places[place-1].arrival.date+" "+places[place-1].arrival.time.split("+")[0]))
                 transitioning = true;
-                transitionVideo.src = places[place--].transitionVideo.split(".")[0] + "-reversed.mp4";
-                placeVideo.src = places[place].video;
-                transitionVideo.style.display = "block";
-                timeout = setTimeout(() => {
-                    transitionVideo.style.display = "none";
+                place--;
+                reversedTransitionVideo.play();
+                reversedTransitionVideo.style.opacity = 1;
+                timeouts.push(setTimeout(() => {placeVideo.src = places[place].video;}, 500));
+                timeouts.push(setTimeout(() => {
+                    placeVideo.play();
+                    reversedTransitionVideo.style.opacity = 0;
+                    transitionVideo.src = places[place+1].transitionVideo;
+                    if (place > 0) setTimeout(() => {reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";}, 500);
                     transitioning = false;
-                }, 5000);
+                }, 5000));
             }
         }
     }
 }
 arrowKeyControl();
+
+function clearTimeouts() {
+    for (t of timeouts)
+        clearTimeout(t);
+    timeouts = [];
+}
