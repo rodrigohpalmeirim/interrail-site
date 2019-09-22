52ed5df0fed269e05c8784a5579f820fd70aa825
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2019-09-22 19:01:35 +0100

    add place announcement

--- earth.js
+++ earth.js
@@ -3,6 +3,7 @@ var placeVideo = document.getElementById("place-video");
 var transitionVideo = document.getElementById("transition-video");
 var reversedTransitionVideo = document.getElementById("reversed-transition-video");
 var transitioning = false;
+var loaded = true;
 var timeouts = [];
 
 setClock(Date.parse(places[place].arrival.date+" "+places[place].arrival.time))
@@ -14,17 +15,27 @@ function arrowKeyControl() {
         e = e || window.event;
         if (e.key == "ArrowRight") {
             if (transitioning) {
+                hideAnnouncement();
+                clearTimeouts();
                 setClock(Date.parse(places[place+1].arrival.date+" "+places[place+1].arrival.time.split("+")[0]))
                 stopClock();
                 placeVideo.src = places[++place].video;
+                loaded = false;
+                placeVideo.oncanplay = function() {
+                    loaded = true;
+                    placeVideo.oncanplay = null;
+                }
                 /* placeVideo.play(); */
-                transitionVideo.style.opacity = 0;
-                reversedTransitionVideo.style.opacity = 0;
-                if (place < places.length-1) transitionVideo.src = places[place+1].transitionVideo;
-                reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";
+                timeouts.push(setTimeout(() => {
+                    transitionVideo.style.opacity = 0;
+                    reversedTransitionVideo.style.opacity = 0;
+                    if (place < places.length-1) transitionVideo.src = places[place+1].transitionVideo;
+                    reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";
+                }, 500));
                 transitioning = false;
-                clearTimeouts();
+                displayPlaceName(places[place].name);
             } else if (place < places.length-1) {
+                hideAnnouncement();
                 fastForward(Date.parse(places[place].departure.date+" "+places[place].departure.time.split("+")[0]), Date.parse(places[place+1].arrival.date+" "+places[place+1].arrival.time.split("+")[0]))
                 transitioning = true;
                 timeouts.push(setTimeout(() => {placeVideo.pause();}, 500));
@@ -32,9 +43,12 @@ function arrowKeyControl() {
                 transitionVideo.style.opacity = 1;
                 transitionVideo.onended = function() {
                     place++;
+                    loaded = false;
+                    displayPlaceName(places[place].name);
                     /* placeVideo.play(); */
                     placeVideo.src = places[place].video;
                     placeVideo.oncanplay = function() {
+                        loaded = true;
                         transitionVideo.style.opacity = 0;
                         if (place < places.length-1) timeouts.push(setTimeout(() => {transitionVideo.src = places[place+1].transitionVideo;}, 500));
                         placeVideo.oncanplay = null;
@@ -46,17 +60,27 @@ function arrowKeyControl() {
             }
         } else if (e.key == "ArrowLeft") {
             if (transitioning) {
+                hideAnnouncement();
+                clearTimeouts();
                 setClock(Date.parse(places[place].arrival.date+" "+places[place].arrival.time.split("+")[0]))
                 stopClock();
                 placeVideo.src = places[place].video;
+                loaded = false;
+                placeVideo.oncanplay = function() {
+                    loaded = true;
+                    placeVideo.oncanplay = null;
+                }
                 /* placeVideo.play(); */
-                transitionVideo.style.opacity = 0;
-                reversedTransitionVideo.style.opacity = 0;
-                transitionVideo.src = places[place+1].transitionVideo;
-                if (place > 0) reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";
+                timeouts.push(setTimeout(() => {
+                    transitionVideo.style.opacity = 0;
+                    reversedTransitionVideo.style.opacity = 0;
+                    transitionVideo.src = places[place+1].transitionVideo;
+                    if (place > 0) reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";
+                }, 500));
                 transitioning = false;
-                clearTimeouts();
+                displayPlaceName(places[place].name);
             } else if (place > 0) {
+                hideAnnouncement();
                 fastForward(Date.parse(places[place].arrival.date+" "+places[place].arrival.time.split("+")[0]), Date.parse(places[place-1].arrival.date+" "+places[place-1].arrival.time.split("+")[0]))
                 transitioning = true;
                 place--;
@@ -66,7 +90,10 @@ function arrowKeyControl() {
                 reversedTransitionVideo.onended = function() {
                     /* placeVideo.play(); */
                     placeVideo.src = places[place].video;
+                    displayPlaceName(places[place].name);
+                    loaded = false;
                     placeVideo.oncanplay = function() {
+                        loaded = true;
                         reversedTransitionVideo.style.opacity = 0;
                         if (place > 0) timeouts.push(setTimeout(() => {reversedTransitionVideo.src = places[place].transitionVideo.split(".")[0] + "-reversed.mp4";}, 500));
                         placeVideo.oncanplay = null;
--- ui.js
+++ ui.js
@@ -1,17 +1,18 @@
 
 function displayDay(day) {
-    text = document.querySelector("#day-text");
-    announcement = document.querySelector("#day-announcement");
+    let text = document.querySelector("#day-text");
+    let announcement = document.querySelector("#announcement");
     
+    document.querySelector(".ml1").style.opacity = 1;
     announcement.style.display = "block";
+    announcement.style.backgroundColor = "#1f273c";
     text.textContent = "DAY " + day;
-    var textWrapper = document.querySelector('.ml1 .letters'); // Wrap every letter in a span
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    let textWrapper = document.querySelector('.ml1 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>"); // Wrap every letter in a span
 
     setTimeout(() => {announcement.style.opacity = 1;}, 10);
 
-    setTimeout(() => {
-        /* text.style.opacity = 1; */
+    timeouts.push(setTimeout(() => {
         anime.timeline({})
             .add({
                 targets: '.ml1 .letter',
@@ -36,13 +37,89 @@ function displayDay(day) {
                 easing: "easeOutExpo",
                 delay: 1000
             }) */;
-    }, 500);
-    setTimeout(() => {announcement.style.opacity = 0;}, 2500);
-    setTimeout(() => {
-        announcement.style.display = "none";
-        text.style.opacity = 0;
-    }, 3000);
-    
+    }, 500));
+
+    timeouts.push(setTimeout(() => {hideAnnouncement();}, 2500));
 }
 
-setTimeout(() => {displayDay(1);}, 1000);
+setTimeout(() => {displayPlaceName(places[0].name);}, 2000);
+
+
+function displayPlaceName(name) {
+    let text = document.querySelector("#place-name");
+    let announcement = document.querySelector("#announcement");
+
+    document.querySelector(".ml11").style.opacity = 1;
+    announcement.style.display = "block";
+    announcement.style.backgroundColor = "#206490";
+    document.querySelector(".spinner").style.opacity = 0;
+    text.textContent = name;
+
+    setTimeout(() => {announcement.style.opacity = 1;}, 10);
+
+    var textWrapper = document.querySelector('.ml11 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"); // Wrap every letter in a span
+
+    timeouts.push(setTimeout(() => {
+        anime.timeline({})
+            .add({
+                targets: '.ml11 .line',
+                scaleY: [0,1],
+                opacity: [0.5,1],
+                easing: "easeOutExpo",
+                duration: 700
+            }).add({
+                targets: '.ml11 .line',
+                translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+                easing: "easeOutExpo",
+                duration: 700,
+                delay: 100
+            }).add({
+                targets: '.ml11 .letter',
+                opacity: [0,1],
+                easing: "easeOutExpo",
+                duration: 600,
+                offset: '-=775',
+                delay: (el, i) => 30 * (i+1)
+            }).add({
+                targets: '.ml11 .line',
+                opacity: 0,
+                duration: 700,
+                easing: "easeOutExpo"
+            });
+        }, 500));
+        
+        timeouts.push(setTimeout(() => {
+            if (loaded) {
+                timeouts.push(setTimeout(() => {hideAnnouncement();}, 500));
+            } else {
+                anime.timeline().add({
+                    targets: '.spinner',
+                    opacity: [0,1],
+                    duration: 300,
+                    easing: "easeInExpo"
+                });
+                timeouts.push(setTimeout(() => {
+                    let interval = setInterval(() => {
+                        if (loaded) {
+                            hideAnnouncement();
+                            clearInterval(interval);
+                        }
+                    }, 100);
+                }, 500));
+            }
+        }, 2500));
+}
+
+function hideAnnouncement() {
+    let announcement = document.querySelector("#announcement");
+    announcement.style.opacity = 0;
+    timeouts.push(setTimeout(() => {
+        announcement.style.display = "none";
+        document.querySelector(".ml1").style.opacity = 0;
+        document.querySelector(".ml11").style.opacity = 0;
+        document.querySelector(".spinner").style.opacity = 0;
+        document.querySelector(".line1").style.opacity = 0;
+        document.querySelector(".line2").style.opacity = 0;
+    }, 500));
+}
