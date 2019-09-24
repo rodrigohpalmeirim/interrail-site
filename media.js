5d276478dff112cf9885f319a9bc5694b59cd211
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2019-09-24 12:48:11 +0100

    add video compatibility

--- media.js
+++ media.js
@@ -1,17 +1,31 @@
-function big(src) {
-    document.getElementById("big-photo").src = src;
+function big(src, type) {
     document.getElementById("media-viewer").style.display = "block";
     setTimeout(() => {document.getElementById("media-viewer").style.opacity = 1;}, 1);
-    document.getElementById("big-photo").style.display = "block";
-    setTimeout(() => {document.getElementById("big-photo").style.opacity = 1;}, 1);
+    if (type == "photo") {
+        document.getElementById("big-video").src = "";
+        document.getElementById("big-video").style.display = "none";
+        document.getElementById("big-photo").src = src;
+        document.getElementById("big-photo").style.display = "block";
+        setTimeout(() => {document.getElementById("big-photo").style.opacity = 1;}, 1);
+    } else if (type == "video") {
+        document.getElementById("big-photo").src = "";
+        document.getElementById("big-photo").style.display = "none";
+        document.getElementById("big-video").src = src;
+        document.getElementById("big-video").style.display = "block";
+        setTimeout(() => {document.getElementById("big-video").style.opacity = 1;}, 1);
+    }
 
     document.onkeydown = function (e) {
         e = e || window.event;
         if (e.key == "Escape") {
+            document.getElementById("big-photo").src = "";
+            document.getElementById("big-video").src = "";
             document.getElementById("media-viewer").style.opacity = 0;
             setTimeout(() => {document.getElementById("media-viewer").style.display = "none";}, 200);
             document.getElementById("big-photo").style.opacity = 0;
             setTimeout(() => {document.getElementById("big-photo").style.display = "none";}, 200);
+            document.getElementById("big-video").style.opacity = 0;
+            setTimeout(() => {document.getElementById("big-video").style.display = "none";}, 200);
             arrowKeyControl();
         }
     }
@@ -23,8 +37,11 @@ function updateMedia(startDate, endDate) {
     mediaViewer = document.querySelector("#media-viewer");
     media.innerHTML = "";
     for (let i=0; i<points.length && (new Date(points[i].date+" "+points[i].time)<endDate); i++) {
-        if (new Date(points[i].date+" "+points[i].time)>startDate && (points[i].type == "photo" || points[i].type == "video")) {
-            media.innerHTML += `<img class="thumbnail" src="`+"media/"+points[i].file+`" onclick="big('`+"media/"+points[i].file+`')">`;
+        if (new Date(points[i].date+" "+points[i].time)>startDate) {
+            if (points[i].type == "photo")
+                media.innerHTML += `<img class="thumbnail" src="`+"media/"+points[i].file+`" onclick="big('`+"media/"+points[i].file+`', 'photo')">`;
+            else if (points[i].type == "video")
+                media.innerHTML += `<video class="thumbnail" src="`+"media/"+points[i].file+`" onclick="big('`+"media/"+points[i].file+`', 'video')"></video>`;
         }
     }
     if (media.childElementCount == 0)
