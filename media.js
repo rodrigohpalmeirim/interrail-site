2e6d5797033cb43e63a040a5cb096dd11a2d5ec0
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2019-09-24 12:14:49 +0100

    add media pane auto resize logic

--- media.js
+++ media.js
@@ -19,12 +19,23 @@ function big(src) {
 
 function updateMedia(startDate, endDate) {
     media = document.querySelector("#media");
+    mediaPane = document.querySelector("#media-pane");
+    mediaViewer = document.querySelector("#media-viewer");
     media.innerHTML = "";
-    console.log((0<points.length) && (new Date(points[0].date+" "+points[0].time)<endDate));
     for (let i=0; i<points.length && (new Date(points[i].date+" "+points[i].time)<endDate); i++) {
-        console.log("   "+(new Date(points[i].date+" "+points[i].time)>startDate));
         if (new Date(points[i].date+" "+points[i].time)>startDate && (points[i].type == "photo" || points[i].type == "video")) {
             media.innerHTML += `<img class="thumbnail" src="`+"media/"+points[i].file+`" onclick="big('`+"media/"+points[i].file+`')">`;
         }
+    }
+    if (media.childElementCount == 0)
+        mediaPane.style.width = "0%";
+    else if (media.childElementCount < 5) {
+        mediaPane.style.width = "15%";
+        mediaViewer.style.width = "85%";
+        media.style.columnCount = 1;
+    } else {
+        mediaPane.style.width = "30%";
+        mediaViewer.style.width = "70%";
+        media.style.columnCount = 2;
     }
 }
